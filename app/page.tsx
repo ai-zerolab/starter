0be42c1fd6c8f89664081c290c92ea4725e91263
Wh1isper <9573586@qq.com>
2025-03-14 15:14:31 +0800

    Switch to Arco title

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
         <div className="w-full min-h-screen flex items-center justify-center bg-white dark:bg-black transition-colors duration-200 flex-col p-4 gap-[32px]">
             <div className="text-center text-gray-900 dark:text-gray-100 p-4">
                 <h1 className="text-4xl md:text-5xl font-semibold mb-4 tracking-tight">
-                    Welcome to your new Onlook website
+                    Welcome to your new Arco website
                 </h1>
                 <p className="text-xl md:text-2xl text-gray-800 dark:text-gray-300">
                     This is a blank template – Feel free to edit it!
